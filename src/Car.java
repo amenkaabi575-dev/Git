6240dc9986334a0bcbe449af3ac4bef055284a13
Amenallah Kaabi <amenkaabi123@gmail.com>
2026-05-01 18:56:00 +0100

    Version 7

--- src/Car.java
+++ src/Car.java
@@ -1,6 +1,23 @@
 public class Car extends Vehicle{
 
 
-    
+    private double weigh;
 
+    public Car() {
+    }
+
+    public Car(long id, String brand, String model, double weigh) {
+        super(id, brand, model);
+        this.weigh = weigh;
+    }
+
+    public double getWeigh() {
+        return weigh;
+    }
+
+    public void setWeigh(double weigh) {
+        this.weigh = weigh;
+    }
+
+    
 }
